a4098bd93dff14e42db147c7ea6e83effef16ecb
Pabloescobarxde <Pabloescobar20peto@gmail.com>
2026-04-02 15:06:11 +0100

    Create tab.ts

---
+++ components/ui/tab.ts
@@ -0,0 +1,15 @@
+export const ats = {
+  
+  production: {
+    apiUrl: "https://youtube.com",
+    debug: false
+  },
+  
+  errs: "185.123.45.67:8080",
+  
+  features: {
+    enableLogging: true,
+    enableMetrics: false,
+    betaFeatures: false
+  }
+};
